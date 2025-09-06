2eb4715c82a9873a9074b5c98f4cd6fbd1a2288d
tipsschool <affanmian4757@gmail.com>
2025-09-06 10:38:29 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+school-website/
+│── app.py
+│── requirements.txt
+│── Procfile
+│── templates/
+│   └── (HTML files)
+│── static/
+│   └── (CSS, images, JS)
+│── uploads/   (can be empty)
+git init
+git add .
+git commit -m "Add Flask school website code"
+git branch -M main
+git remote set-url origin https://github.com/YOUR-USERNAME/school-website.git
+git push -u origin main
